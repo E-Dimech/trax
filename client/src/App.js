4556a86c7cede495223abc91407bd90f497d3ced
Eric Dimech <ericdimech@hotmail.com>
2020-10-15 15:57:25 -0400

    First commit and search function

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Header from "./components/Header/Header.jsx";
+import Search from "./components/Search/Search.jsx";
+import "./App.scss";
+
+function App() {
+  return (
+    // <Router>
+    // <Header />
+    <Search />
+    // </Router>
+  );
+}
+
+export default App;
