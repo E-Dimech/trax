55b384518b258e0dbf77d43a4359d538294097d1
Eric Dimech <ericdimech@hotmail.com>
2020-10-15 16:59:29 -0400

    Update Port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
